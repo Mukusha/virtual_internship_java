a93009b43e20d8b059144c70448cd1e31623e0ec
Mukusha <sveta.gurinovitch@yandex.ru>
2024-08-20 17:06:09 +0300

    STEP 013 - DONE

--- internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,12 +1,33 @@
 package org.javaguru.travel.insurance.core;
 
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
+import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TravelCalculatePremiumServiceImplTest {
+    private TravelCalculatePremiumService service = new TravelCalculatePremiumServiceImpl();
+
 
     @Test
-    public void deleteMe() {
+    public void calculatePremiumTest() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
+                "Ivan",
+                "Ivanov",
+                new Date(),
+                new Date()
+        );
 
+        TravelCalculatePremiumResponse response = service.calculatePremium(request);
+
+        assertEquals(response.getPersonFirstName(), request.getPersonFirstName());
+        assertEquals(response.getPersonLastName(), request.getPersonLastName());
+        assertEquals(response.getAgreementDateFrom(), request.getAgreementDateFrom());
+        assertEquals(response.getAgreementDateTo(), request.getAgreementDateTo());
     }
 
 }
