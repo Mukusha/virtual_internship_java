40f77164eddfacb37c7e7f4cb823c7d02e10017f
Mukusha <sveta.gurinovitch@yandex.ru>
2024-08-22 19:09:19 +0300

    STEP 027 - DONE

--- internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ internship/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -1,5 +1,7 @@
 package org.javaguru.travel.insurance.rest;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.javaguru.travel.insurance.core.TravelCalculatePremiumService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,20 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 @RequestMapping("/insurance/travel")
 public class TravelCalculatePremiumController {
 
-	private final TravelCalculatePremiumService calculatePremiumService;
+    private final TravelCalculatePremiumService calculatePremiumService;
 
-	TravelCalculatePremiumController(TravelCalculatePremiumService calculatePremiumService) {
-		this.calculatePremiumService = calculatePremiumService;
-	}
-
-	@PostMapping(path = "/",
-			consumes = "application/json",
-			produces = "application/json")
-	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
-		return calculatePremiumService.calculatePremium(request);
-	}
+    @PostMapping(path = "/",
+            consumes = "application/json",
+            produces = "application/json")
+    public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+        return calculatePremiumService.calculatePremium(request);
+    }
 
 }
